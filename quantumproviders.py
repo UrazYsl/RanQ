669656bea7db6b5d64b7aeb8d47eb5e2ed041836
UrazYsl <urazysl@gmail.com>
2026-07-01 00:41:44 -0400

    Created the abstract and classical provider class for quantum providers.

---
+++ quantumproviders.py
@@ -0,0 +1,25 @@
+from random import randint
+class QuantumProvider:
+
+    name: str
+
+    def __init__(self, name: str):
+        self.name = name
+    
+    def generate_qubits(self, groups: dict[str, int]) -> dict[str, str]:
+        raise NotImplementedError
+    
+class ClassicalProvider(QuantumProvider):
+
+    def __init__(self):
+        super().__init__("classical")
+    
+    def generate_qubits(self, groups: dict[str, int]) -> dict[str, str]:
+        bits = {}
+        bitstr = ""
+        for attribute in groups:
+            for i in range(groups.get(attribute)):
+                bitstr += str(randint(0,1))
+            bits[attribute] = bitstr
+            bitstr = ""
+        return bits
